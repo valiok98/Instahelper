c5dc174f954dd69aa51ca409fc0709f6e47ad5ab
Valio98 <valentinkostadinov@w203-61-v4.eduroam.dynamic.rbg.tum.de>
2019-07-02 21:49:18 +0200

    Support for closing last IG tab or reopen Index tab.

--- chrome_extension/js/background/index.js
+++ chrome_extension/js/background/index.js
@@ -1,15 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.browserAction.onClicked.addListener(function (tab) {
-
-        chrome.tabs.create({
-            url: 'https://www.instagram.com/venturemashup/'
+    chrome.browserAction.onClicked.addListener(tab => {
+        chrome.windows.getCurrent({ populate: true }, win => {
+            // Check against the index.html from our chrome extension.
+            const IndexRegex = RegExp(`${chrome.runtime.id}/html/index.html`);
+            for (const tab of win.tabs) {
+                // Return if we already have a running index.html instance.
+                if (IndexRegex.test(tab.url)) {
+                    return;
+                }
+            }
+            // Open the tab through javascript, window is the glbal object.
+            window.open('./html/index.html');
         });
-
-        chrome.tabs.create({
-            url: './html/index.html'
-        });
-
     });
-
 });
 
--- chrome_extension/js/content/fetch-followers.js
+++ chrome_extension/js/content/fetch-followers.js
@@ -1,19 +1,62 @@
 const h1 = document.querySelector('h1'),
     button = document.querySelector('button');
 
-const IgRegex = RegExp('instagram\.com');
+const InstagramRegex = RegExp(`www\.instagram\.com`);
 
-let Tab = {
-    id: -1,
-    index: -1
-}
+chrome.windows.getCurrent({ populate: true }, win => {
+    // Check against the index.html from our chrome extension.
+    for (const tab of win.tabs) {
+        // Leave if we already have an Instragram instance.
+        if (InstagramRegex.test(tab.url)) {
+            return;
+        }
+    }
+    // Open an Instagram instance, if there are no others.
+    chrome.tabs.create({
+        url: 'https://www.instagram.com/'
+    }, tab => {
+        chrome.tabs.executeScript(tab.id, {
+            file: './js/injectables/get-followers.js'
+        });
+    });
+});
+
+// Handle closing of tabs.
+chrome.tabs.onRemoved.addListener((tabId, moveInfo) => {
+    chrome.tabs.query({
+        url: [
+            'https://www.instagram.com/',
+            'https://www.instagram.com/*/'
+        ]
+    }, instaTabs => {
+        // Check if we closed the last Instagram instance and close index.
+        if (instaTabs.length === 0) {
+            chrome.tabs.query({
+                url: `chrome-extension://${chrome.runtime.id}/html/index.html`
+            }, indexTabs => {
+                for (const indexTab of indexTabs) {
+                    chrome.tabs.remove(indexTab.id);
+                }
+            });
+        }
+    });
+});
 
 chrome.tabs.onMoved.addListener((tabId, moveInfo) => {
     chrome.tabs.query({
-        url: 'https://*.instagram.com/*'
+        url: [
+            'https://www.instagram.com/',
+            'https://www.instagram.com/*/'
+        ]
     }, tabs => {
         if (tabs.length !== 0) {
-            h1.textContent = tabs[0].index;
+
+            h1.textContent = 'HEllo';
+
+            chrome.tabs.executeScript(tabs[0].id, {
+                file: '../injectables/get-followers.js'
+            });
+
         }
     });
 });
